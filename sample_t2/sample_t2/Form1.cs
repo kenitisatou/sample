10120f66a7622a35507b80667f496b18a06d4d53
fujii <t12306063@gunma-u.ac.jp>
2015-04-14 00:38:54 +0900

    038

--- sample_t2/sample_t2/Form1.cs
+++ sample_t2/sample_t2/Form1.cs
@@ -19,7 +19,7 @@ namespace sample_t2
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("button test");
+            MessageBox.Show("button test2");
         }
     }
 }
